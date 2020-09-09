f2b52995871cb9f0bcfe855455b1ba060a1afd64
Griffin <cougargriff@gmail.com>
2020-09-09 11:33:16 -0700

    author tooltip showing

--- app.js
+++ app.js
@@ -2,64 +2,68 @@
 var express = require("express");
 const fetch = require("node-fetch");
 var app = express();
-require('dotenv').config()
+require("dotenv").config();
 
-var Projects = require('./src/Projects.js')
-var Users = require('./src/Users.js')
-var getPage = Projects.getPageGen()
-var getUser = Users.getUserGen()
+var Projects = require("./src/Projects.js");
+var Users = require("./src/Users.js");
+var getPage = Projects.getPageGen();
+var getUser = Users.getUserGen();
 
 const port = 3000;
 app.use(express.static("public"));
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.set('views', './views')
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  var page = await getPage(fetch, 1)
-  
-  console.log('Received Page')
+  var page = await getPage(fetch, 1);
 
-  page = await Promise.all(page.map(async (prj) => {
-    var ownerName = await getUser(fetch, prj.owner_id)
-    return {
-      ...prj,
-      owner: ownerName
-    }
-  }))
-  console.log('Finished Getting Meta Data')
+  console.log("Received Page");
 
-  page = JSON.stringify(page)
+  page = await Promise.all(
+    page.map(async (prj) => {
+      var author = await getUser(fetch, prj.owner_id);
+      return {
+        ...prj,
+        owner: author,
+      };
+    })
+  );
+  console.log("Finished Getting Meta Data");
+
+  page = JSON.stringify(page);
   res.render("index", {
     title: "Hackaday Projects",
     prjs: page,
-    currPage: 1
+    currPage: 1,
   });
 });
 
-app.get('/:page', async (req, res) => {
-  const pg = req.params.page
-  var page = await getPage(fetch, pg)
-  console.log('Received Page')
+app.get("/:page", async (req, res) => {
+  const pg = req.params.page;
+  var page = await getPage(fetch, pg);
+  console.log("Received Page");
 
-  page = await Promise.all(page.map( async (prj) => {
-    var ownerName = await getUser(fetch, prj.owner_id)
-    return {
-      ...prj,
-      owner: ownerName
-    }
-  }))
-  console.log('Finished Getting Meta Data')
+  page = await Promise.all(
+    page.map(async (prj) => {
+      var author = await getUser(fetch, prj.owner_id);
+      return {
+        ...prj,
+        owner: author,
+      };
+    })
+  );
+  console.log("Finished Getting Meta Data");
 
-  page = JSON.stringify(page)
+  page = JSON.stringify(page);
   res.render("index", {
     title: "Hackaday Projects",
     prjs: page,
-    currPage: parseInt(pg)
+    currPage: parseInt(pg),
   });
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
--- src/Users.js
+++ src/Users.js
@@ -1,28 +1,25 @@
-
 const getUserGen = () => {
-
   /*
     Closure over this map serves as a cache to remember
     which project pages we have visited.
   */
-  userList = {}
+  userList = {};
 
   const getUser = async (fetch, id) => {
-    const url_prefix = process.env.API_URL
-    const key = process.env.API_KEY
-    const url = `${url_prefix}users/${id}?api_key=${key}`
-    var res = {}
+    const url_prefix = process.env.API_URL;
+    const key = process.env.API_KEY;
+    const url = `${url_prefix}users/${id}?api_key=${key}`;
+    var res = {};
     if (userList[id] == undefined) {
-      res = (await fetch(url).then(res => res.json()))
-      userList[res.id] = res.screen_name
-      res = res.screen_name
+      res = await fetch(url).then((res) => res.json());
+      userList[res.id] = res;
+      res = res;
     } else {
-      res = userList[id]
+      res = userList[id];
     }
-    return res
-  }
-  return getUser
-}
+    return res;
+  };
+  return getUser;
+};
 
-
-module.exports = { getUserGen }
+module.exports = { getUserGen };
